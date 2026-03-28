7ef14031b9c0f53e2c8742bf0369d12c2f8f72d9
CMCGEEHAN <c.mcgeehan91@gmail.com>
2026-03-28 20:44:56 +0000

    updated to analysis. added record id required for input data to provide clean feedback

--- ELT/LOAD_CLIENT_AND_FILE.sql
+++ ELT/LOAD_CLIENT_AND_FILE.sql
@@ -20,9 +20,20 @@ USE DATABASE CUSTOMER;
 LIST @CUSTOMER_NAMES;
 
 INSERT INTO CUSTOMER.RAW.PERSON_INPUT(CLIENT_ID, BATCH_ID,RECORD_ID, FIRST_NAME, LAST_NAME, ZIP)
-SELECT 1, 1, NULL,  t.$1, t.$2, t.$3 FROM @CUSTOMER_NAMES (pattern=>'new_sample.csv.gz') t;
+SELECT 1, 1, t.$1,  t.$2, t.$3, t.$4 FROM @CUSTOMER_NAMES (pattern=>'updated_sample.csv.gz') t;
 
 REMOVE @CUSTOMER_NAMES;
+-- Refresh dynamic to pull data downstream
+alter dynamic table CUSTOMER.ANALYTICS.PERSON_INPUT_GENDER_BASELINE refresh;
+
+-- SELECT name, database_name, schema_name, data_timestamp, state, state_code, state_message, refresh_trigger
+-- FROM SNOWFLAKE.ACCOUNT_USAGE.DYNAMIC_TABLE_REFRESH_HISTORY
+-- WHERE name = 'FIRST_NAME_GENDER_QUANTITY_YEAR'
+--   AND database_name = 'DATA_PROVIDER_SSA'
+-- ORDER BY data_timestamp DESC
+-- LIMIT 10;
+
+SQL compilation error: Change tracking is not enabled or has been missing for the time range requested on table 'DATA_PROVIDER_SSA.RAW.FIRST_NAME_GENDER_QUANTITY_YEAR'.
 
 -- Step 1: snow connection test
 -- Step 2: snow sql
@@ -35,7 +46,8 @@ REMOVE @CUSTOMER_NAMES;
 -- Step 4: Make sure files aren't currently in Stage: LIST @CUSTOMER_NAMES;
 -- Step 4a: If files are in Stage: REMOVE @CUSTOMER_NAMES;
 
--- Step 5: Using actual local path: PUT file://C:\Users\Charles\Documents\Inferlytica\test_client_data\new_sample.csv @CUSTOMER_NAMES;
+-- Step 5: Using actual local path: PUT file://C:\Users\Charles\Documents\Inferlytica\test_client_data\updated_sample.csv @CUSTOMER_NAMES;
 -- Step 6: COPY INTO CUSTOMER.RAW.FIRST_NAME_GENDER_QUANTITY_YEAR FROM @CUSTOMER_NAMES;
 -- Step 7: Verify Row count appears accurate based on results in Step 6 (empty rows are skipped I think)
 -- Step 8: Clean up Stage: REMOVE @CUSTOMER_NAMES;
+-- Step 9: Force downstream refresh: alter dynamic table CUSTOMER.ANALYTICS.PERSON_INPUT_GENDER_BASELINE refresh;
--- INFRASTRUCTURE/CUSTOMER LAYER/Build RAW Customer Layer.sql
+++ INFRASTRUCTURE/CUSTOMER LAYER/Build RAW Customer Layer.sql
@@ -5,9 +5,13 @@ CREATE SCHEMA CUSTOMER.RAW;
 create or replace transient table CUSTOMER.RAW.PERSON_INPUT (
     CLIENT_ID NUMBER(32,0),
     BATCH_ID NUMBER(32, 0),
-    RECORD_ID varchar,
+    RECORD_ID varchar NOT NULL,
     FIRST_NAME varchar,
     LAST_NAME varchar,
     ZIP varchar,
     LOAD_TS timestamp_ntz default current_timestamp()
 );
+
+
+UPDATE CUSTOMER.RAW.PERSON_INPUT
+SET RECORD_ID = UUID_STRING();
