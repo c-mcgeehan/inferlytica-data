5bc1042ce62e2e529820f90c08da6a3bdbad1efc
CMCGEEHAN <c.mcgeehan91@gmail.com>
2026-04-03 02:45:19 +0000

    proc planning for notifs

--- DB_CUSTOMER/FILE_PROCESSING/PROCEDURES/Send Client Batch Notifications.sql
+++ DB_CUSTOMER/FILE_PROCESSING/PROCEDURES/Send Client Batch Notifications.sql
@@ -8,7 +8,8 @@ BEGIN
     --PREP stream data
     CREATE OR REPLACE TEMP TABLE TMP_CLIENT_BATCH_NOTIFICATIONS AS
     SELECT
-       CLIENT_ID,
+        ID,
+        CLIENT_ID,
         BATCH_ID,
         APP_FILE_ID,
         APP_ORGANIZATION_ID,
@@ -19,6 +20,10 @@ BEGIN
         LAST_ATTEMPT_TS,
         LAST_ERROR,
     FROM CUSTOMER.FILE_PROCESSING.CLIENT_BATCH_NOTIFICATIONS_STREAM S;
+
+    -- Send notification to supabase per record
+    -- Updates notification (CUSTOMER.FILE_PROCESSING.CLIENT_BATCH_NOTIFICATIONS) records on table to be marked as failed, succeeded etc...
+    -- MAKE THIS A PYTHON PROC that loops through and makes the api requests then gets a response and handles one at a time
     
     RETURN 'Processed ' || (SELECT COUNT(*) FROM TMP_CLIENT_BATCH_NOTIFICATIONS) || ' stream rows.';
 END;
